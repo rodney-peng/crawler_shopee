1e324bd0b7d79494e0953a8f0ee596951978111f
Rodney Peng <rodney.peng@gmail.com>
2021-01-08 14:43:16 +0800

    1. refactor classes to try to remove dependencies of global objects 2. use chrome driver's execute_cdp_cmd() to set cookies before get() request,    thus no need to refresh once cookies are loaded. Requires selenium >= 3.13.0 3. update README

--- README.md
+++ README.md
@@ -3,78 +3,24 @@
 
 Shopee coin getter is a script to collect daily shopee coins.
 
-![alt text](https://raw.githubusercontent.com/charlie0227/crawler_shopee/master/readme/overall-1.png)
-## dependencies
-    python==3.6
-    selenium==3.8.0
-    
-## Installation
- [Docker](https://www.docker.com)
- 
-## Getting Started
-    git clone https://github.com/charlie0227/crawler_shopee.git
-    cd crawler_shopee
-    cp env.py.sample env.py 
-    
-Filled in your username and password in env.py
+![alt text](https://raw.githubusercontent.com/rodney-peng/crawler_shopee/master/readme/overall-1.png)
+## Dependencies
 
-    text_username = "<your shopee username>" 
-    text_password = "<your shopee password>"
-    cookie_name = "cookie.pkl"
-    
+    python>=3.6
+    selenium>=3.13.0
+
 ## Usage
 
-Simply run command
+clone the repository,
+copy env.py.sample to env.py and optionally edit the cookie name in env.py:
 
-    docker run -it --rm -v <your-path>/crawler_shopee:/code charlie27/python36-selenium-chromedriver sh -c "python main.py"
-    
-Or build by yourself
+    cookie_name = "cookie.pkl"
 
-    docker build -t shopee:latest .
-    docker run -it --rm -v <your-path>/crawler_shopee:/code shopee sh -c "python main.py"
-    
-You'll need to enter SMS authenticate first time if used password to login
+Please note that text_username and text_password are no longer used and kept only for compatibility.
 
-![alt text](https://raw.githubusercontent.com/charlie0227/crawler_shopee/master/readme/SMS.png)
+Then run:
 
-    Please Enter SMS code in 60 seconds: 
-
-crontab
-
-    0 0 * * * /usr/bin/docker run --rm -v <your-path>/crawler_shopee:/code shopee sh -c "python main.py" >/dev/null
-
-## Running the tests
-
-Explain how to run the unit test
-
-    docker run -it --rm -v .:/code charlie27/python36-selenium-chromedriver sh -c "python unit_test.py"
-    
-## Method
-
-__checkPopModal()__
-
-Auto close the advertisement modal shopee show first time
-
-__checkLogin()__
-
-check is login or not
-
-__loginByCookie()__
-
-First, check your cookie is able to login, if success goto clickCoin
-
-__loginByPass()__
-
-Second, use your account and password to login
-
-__checkSMS()__
-
-Third, if you login by password first time, you'll need to pass SMS authenticate.
-
-__clickCoin()__
-
-Last, goto https://shopee.tw/shopee-coins-internal/?scenario=1 to own your shopee daily coin after login.
-
+    python main.py
 
 ## License
 
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pickle
 import logging
 import datetime
 import inspect
-from env import *
 from time import gmtime, strftime, sleep
 from selenium.common.exceptions import TimeoutException
 from selenium import webdriver
@@ -13,13 +12,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.webelement import WebElement
 
+from env import *
+def initConfigClass(config):
+    config.text_username = text_username
+    config.text_password = text_password
+    config.cookie_name = cookie_name
+    config.init_logger()
+    return config
+
+@initConfigClass
 class Config:
 
     DEBUG = False
     LOGGING_PATH = 'log'
     WAIT_TIMEOUT = 5
-    elements_by_css = {
+    configs = {
         "POP_MODAL": ".shopee-popup__close-btn",
         "AVATAR": ".shopee-avatar",
         "NAV_LOGIN_MODAL": ".navbar__link--account",
@@ -32,176 +41,217 @@ class Config:
         "COIN_NOW": ".check-box .total-coins",
         "GET_COIN": ".check-box .check-in-tip",
         "COIN_REGULAR": ".check-box .top-btn.Regular",
+
         "COIN_VALUE": "._37aS8q",
-        "COIN_BUTTON": "._1Puh5H"
-    }
-    elements_by_name = {
+        "COIN_BUTTON": "._1Puh5H",
+
         "LOGIN_USER": "loginKey",
-        "LOGIN_PASS": "password"
-    }
-    urls = {
-        "INDEX": "https://shopee.tw",
+        "LOGIN_PASS": "password",
+
+        "HOME_URL": "https://shopee.tw",
         "COIN_PAGE": "https://shopee.tw/shopee-coins"
     }
     path = os.path.dirname(os.path.abspath(__file__))
 
+    @classmethod
+    def get(cls, name):
+        return cls.configs.get(name)
 
-class Logger(Config):
-    def __init__(self):
-        path = os.path.join(self.path, self.LOGGING_PATH)
-        path = "{}/{}".format(path, datetime.datetime.now().strftime("shopee.%Y-%m.log"))
-#        if not os.path.exists(path):
-#            os.makedirs(path)
-        logging_level = logging.DEBUG if self.DEBUG else logging.INFO
-        logger = logging.getLogger()
-        logger.setLevel(logging_level)
-        formatter = logging.Formatter('[%(filename)s:%(lineno)s - %(funcName)20s() ] %(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    logger = None
+
+    @classmethod
+    def init_logger(cls, name=None):
+        log_dir   = cls.LOGGING_PATH
+        log_file  = "{}/{}".format(log_dir, datetime.datetime.now().strftime("shopee.%Y-%m.log"))
+        log_level = logging.DEBUG if cls.DEBUG else logging.INFO
+
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        logger = logging.getLogger(name)
+
+        logger.setLevel(log_level)
+
         ch = logging.StreamHandler()
+        formatter = logging.Formatter('[%(filename)s:%(lineno)s - %(funcName)20s() ] %(asctime)s %(name)-12s %(levelname)-8s %(message)s')
         ch.setFormatter(formatter)
-        fh = logging.FileHandler(path)
+
+        fh = logging.FileHandler(log_file)
         fh.setFormatter(formatter)
+
         logger.addHandler(ch)
         logger.addHandler(fh)
-        self.logger = logger
-
-    def get_logger(self):
-        return self.logger
+        cls.logger = logger
 
 
-logger = Logger().get_logger()
+class Driver(webdriver.Chrome):
 
-class Driver(Config):
+    logger = Config.logger
 
-    def __init__(self, width, height):
+    def __init__(self, width, height, headless=True):
 
         chrome_options = Options()
         # Hide the chromedriver
-        if not self.DEBUG or self.path == '/code':
-#            chrome_options.add_argument('--headless')
+        if headless:
+            chrome_options.add_argument('--headless')
             chrome_options.add_argument('--start-maximized')
             chrome_options.add_argument('disable-infobars')
             chrome_options.add_argument('--disable-extensions')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
-        self.driver = webdriver.Chrome('chromedriver',options=chrome_options)
-        self.driver.set_window_size(width, height)
-        self.path = os.path.dirname(os.path.abspath(__file__))
-        logger.debug("Init driver done.")
+        super().__init__('chromedriver',options=chrome_options)
+        self.set_window_size(width, height)
+        self.logger.debug("Init driver done.")
 
-    def saveCookie(self, cookieName):
-        with open(self.path + '/' + cookieName, 'wb') as filehandler:
-            pickle.dump(self.driver.get_cookies(), filehandler)
-        logger.debug("Save cookie to {}".format(cookieName))
+    def preloadCookie(self, cookieFile):
+        with open(cookieFile, 'rb') as filehandler:
+            # Enables network tracking so we may use Network.setCookie method
+            self.execute_cdp_cmd('Network.enable', {})
 
-    def loadCookie(self, cookieName):
-        with open(self.path + '/' + cookieName, 'rb') as cookiesfile:
-            for cookie in pickle.load(cookiesfile):
-                self.driver.add_cookie(cookie)
+            for cookie in pickle.load(filehandler):
+                # Fix issue Chrome exports 'expiry' key but expects 'expire' on import
+                if 'expiry' in cookie:
+                    cookie['expires'] = cookie['expiry']
+                    del cookie['expiry']
+                # Set the actual cookie
+                self.execute_cdp_cmd('Network.setCookie', cookie)
 
-    def getRequest(self, url):
-        self.driver.get(self.urls.get(url))
+            # Disable network tracking
+            self.execute_cdp_cmd('Network.disable', {})
 
-    def wait_until(self, method=None, target=None):
-        if method == 'css':
-            selector = By.CSS_SELECTOR
-            target = self.elements_by_css.get(target)
 
+    def saveCookie(self, cookieFile):
+        with open(cookieFile, 'wb') as filehandler:
+            pickle.dump(self.get_cookies(), filehandler)
+
+    def loadCookie(self, cookieFile):
+        with open(cookieFile, 'rb') as filehandler:
+            for cookie in pickle.load(filehandler):
+                self.add_cookie(cookie)
+
+    def waitElementPresence(self, locator, timeout=5):
+        _, target = locator
         try:
-            element = WebDriverWait(self.driver, self.WAIT_TIMEOUT).until(
-                EC.presence_of_element_located((selector, target))
+            element = WebDriverWait(self, timeout).until(
+                EC.presence_of_element_located(locator)
             )
-            if isinstance(element, webdriver.remote.webelement.WebElement):
-                logger.debug( "Found <{} \"{}\">{}</>".format(element.tag_name, target, element.text) )
+            if isinstance(element, WebElement):
+                self.logger.debug( f'Found <{element.tag_name} "{target}">{element.text}</>' )
                 return element
 
         except TimeoutException:
-            logger.info( "Target \"{}\" not found!".format(target) )
+            self.logger.info( f'"{repr(locator)}" not located!' )
 
         except Exception as e:
-            logger.error(repr(e))
+            self.logger.error(repr(e))
 
         return False
 
-    def find(self, method=None, target=None):
-        try:
-            if method == 'css':
-                target = self.elements_by_css.get(target)
-                result = self.driver.find_elements_by_css_selector(target)
-            if method == 'name':
-                target = self.elements_by_name.get(target)
-                result = self.driver.find_elements_by_name(target)
 
-            if (len(result) is 1) and isinstance(result[0], webdriver.remote.webelement.WebElement):
-                logger.debug( "Found <{} \"{}\">{}</>".format(result[0].tag_name, target, result[0].text) )
-                return result[0]
+class Crawler:
 
-        except Exception as e:
-            logger.error(repr(e))
-
-        return False
-
-class Crawler(Driver, Config):
+    config = Config()
+    logger = Config.logger
 
     def __init__(self):
-        super().__init__(1200, 800)
+        self.driver = Driver(1200, 800, (not config.DEBUG) or (config.path == '/code'))
+
+    def getURL(self, name):
+        return self.driver.get(self.config.get(name))
+
+    def waitForClass(self, name):
+        return self.driver.waitElementPresence((By.CSS_SELECTOR, self.config.get(name)), self.config.WAIT_TIMEOUT)
+
+    def getByClass(self, name):
+        return self.driver.find_element_by_css_selector(self.config.get(name))
+
+    def getByName(self, name):
+        return self.driver.find_element_by_name(self.config.get(name))
+
+    def getAllByClass(self, name):
+        return self.driver.find_elements_by_css_selector(self.config.get(name))
+
+    def getAllByName(self, name):
+        return self.driver.find_elements_by_name(self.config.get(name))
 
     def checkPopModal(self):
         try:
             sleep(3)
-            pop = self.find("css", "POP_MODAL")
+            pop = self.getByClass("POP_MODAL")
             pop.click()
-            logger.info("pop modal close")
+            self.logger.info("pop modal close")
         except :
-            logger.info("pop modal not found")
+            self.logger.info("pop modal not found")
 
     def checkLogin(self):
         try:
-            self.wait_until("css", "AVATAR")
-            logger.info("Login Success")
+            self.waitForClass("AVATAR")
+            self.logger.info("Login Success")
             return True
         except Exception as e:
-            logger.info("Login Failed")
+            self.logger.info("Login Failed")
             return False
 
-    def loginByCookie(self, cookieName):
+    def loginByCookie(self):
         try:
-            self.loadCookie(cookieName)
+            self.driver.loadCookie(self.config.cookie_name)
             self.driver.refresh()
-            logger.debug("Use {} to login".format(cookieName))
+            self.logger.debug("Use {} to login".format(self.config.cookie_name))
         except Exception as e:
-            logger.info("{} not found".format(cookieName))
+            self.logger.info("{} not found".format(self.config.cookie_name))
+
+    def preloadCookie(self):
+        try:
+            self.driver.preloadCookie(self.config.cookie_name)
+        except Exception as e:
+            self.logger.info(repr(e))
+            self.logger.info(f'Failed to load cookie from "{self.config.cookie_name}"!')
+
+    def loadCookie(self):
+        try:
+            self.driver.loadCookie(self.config.cookie_name)
+        except Exception as e:
+            self.logger.info(repr(e))
+            self.logger.info(f'Failed to load cookie from "{self.config.cookie_name}"!')
+
+    def saveCookie(self):
+        try:
+            self.driver.saveCookie(self.config.cookie_name)
+        except:
+            self.logger.info(repr(e))
+            self.logger.info(f'Failed to save cookie to "{self.config.cookie_name}"!')
 
     def loginByPass(self):
         try:
             # click to show login modal
-            login_button = self.find("css", "NAV_LOGIN_MODAL")[1]
+            login_button = self.getAllByClass("NAV_LOGIN_MODAL")[1]
             login_button.click()
-            self.wait_until("css", "LOGIN_SUBMIT")
+            self.waitForClass("LOGIN_SUBMIT")
         except Exception as e:
-            logger.error("Login Modal not showing"+repr(e))
+            self.logger.error("Login Modal not showing"+repr(e))
             self.close()
         try:
             # Enter Account & Password
-            accountText = self.find("name", "LOGIN_USER")
-            passwordText = self.find("name", "LOGIN_PASS")
-            submitButtom = self.find("css", "LOGIN_SUBMIT")
+            accountText = self.getByName("LOGIN_USER")
+            passwordText = self.getByName("LOGIN_PASS")
+            submitButtom = self.getByClass("LOGIN_SUBMIT")
 
-            accountText.send_keys(text_username)
-            passwordText.send_keys(text_password)
+            accountText.send_keys(self.config.text_username)
+            passwordText.send_keys(self.config.text_password)
             submitButtom.click()
-            logger.info("Use password to login")
+            self.logger.info("Use password to login")
         except Exception as e:
-            logger.error("Wrong account and password"+repr(e))
+            self.logger.error("Wrong account and password"+repr(e))
             self.close()
             sys.exit(0)
 
     def checkSMS(self):
         try:
             # Check SMS textbox exists
-            self.wait_until("css", "SMS_MODAL")
+            self.waitForClass("SMS_MODAL")
             # Catch text & submit buttom
-            smsText = self.find("css", "SMS_TEXT")
-            smsSubmit = self.find("css", "SMS_SUBMIT")
+            smsText = self.getByClass("SMS_TEXT")
+            smsSubmit = self.getByClass("SMS_SUBMIT")
 
             text_sms = input("Please Enter SMS code in 60 seconds: ")
             smsText.clear()
@@ -210,51 +260,51 @@ class Crawler(Driver, Config):
             # handle sms error
             try:
                 # wait to check if login success
-                self.wait_until("css", "AVATAR")
+                self.waitForClass("AVATAR")
             except:
                 #login failed
-                smsError = self.find("css", "LOGIN_FAILED")
-                if len(smsError) > 0:
-                    logger.error("Sending SMS code "+smsError[0].text)
+                smsError = self.getByClass("LOGIN_FAILED")
+                if smsError:
+                    self.logger.error("Sending SMS code "+smsError.text)
                 else:
-                    logger.error("Sending SMS code Run time out.")
+                    self.logger.error("Sending SMS code Run time out.")
                 self.close()
                 sys.exit(0)
         except Exception as e:
-            logger.info("No need SMS authenticate"+repr(e))
+            self.logger.info("No need SMS authenticate"+repr(e))
 
     def clickCoin(self):
         try:
             # wait for page loading
-            self.getRequest("COIN_PAGE")
-            self.wait_until("css", "COIN_PAGE_READY")
+            self.getURL("COIN_PAGE")
+            self.waitForClass("COIN_PAGE_READY")
             try:
-                self.wait_until("css", "GET_COIN")
+                self.waitForClass("GET_COIN")
                 # get information
-                current_coin = self.find("css", "COIN_NOW")
-                get_coin = self.find("css", "GET_COIN")
+                current_coin = self.getByClass("COIN_NOW")
+                get_coin = self.getByClass("GET_COIN")
                 #show before information
-                logger.info("目前有：" + current_coin.text + " 蝦幣，" + get_coin.text)
+                self.logger.info("目前有：" + current_coin.text + " 蝦幣，" + get_coin.text)
                 #click to get shopee coin
                 get_coin.click()
             except:
                 # Already click
-                logger.info("今天已經獲取過蝦幣")
+                self.logger.info("今天已經獲取過蝦幣")
             #wait for already information display login-check-btn
-            self.wait_until("css", "COIN_REGULAR")
+            self.waitForClass("COIN_REGULAR")
             #show after information
-            current_coin = self.find("css", "COIN_NOW")
-            coin_regular = self.find("css", "COIN_REGULAR")
-            logger.info("目前有：" + current_coin.text + " 蝦幣，" + coin_regular.text)
+            current_coin = self.getByClass("COIN_NOW")
+            coin_regular = self.getByClass("COIN_REGULAR")
+            self.logger.info("目前有：" + current_coin.text + " 蝦幣，" + coin_regular.text)
         except Exception as e:
-            logger.error(repr(e))
+            self.logger.error(repr(e))
             self.close()
 
     def run(self):
-        self.getRequest("INDEX")
+        self.getURL("HOME_URL")
         self.checkPopModal()
         #Use cookie to login
-        self.loginByCookie(cookie_name)
+        self.loginByCookie()
         if not self.checkLogin():
             #Use pass to login
             self.loginByPass()
@@ -264,13 +314,13 @@ class Crawler(Driver, Config):
                     #Login failed
                     self.close()
         #After login, Go to coin page
-        self.saveCookie(cookie_name)
+        self.saveCookie()
         self.clickCoin()
         self.close()
 
     def close(self):
         self.driver.close()
-        logger.info("Program exit")
+        self.logger.info("Program exit")
         sys.exit(0)
 
 from contextlib import contextmanager
@@ -285,29 +335,29 @@ def has_substr( string, substring ):
 
 class ShopeeWeb(Crawler):
 
-    def checkLogin(self):
-        return bool(self.wait_until("css", "AVATAR"))
+    def __init__(self):
+        self.driver = Driver(1200, 800, False)
+
+    def waitLogin(self):
+        return bool(self.waitForClass("AVATAR"))
 
     def login(self):
         ''' login by cookie or manually
         '''
-        self.getRequest("COIN_PAGE")
-        coin_button = self.wait_until("css", "COIN_BUTTON")
+        self.preloadCookie()
+        self.getURL("COIN_PAGE")
+        coin_button = self.waitForClass("COIN_BUTTON")
         if not coin_button:
-            logger.info("Page loading error!")
+            self.logger.info("Page loading error!")
             return False
         if has_substr(coin_button.text, '登入'):
-            self.loginByCookie(cookie_name)
-            if not self.checkLogin():
-                coin_button.click()
-                input('Please login before proceed! Press <enter> to continue!')
-            else:
-                logger.debug( "Login by cookie!" )
-        if self.checkLogin():
-            logger.debug("Login successful!")
-            self.saveCookie(cookie_name)
+            coin_button.click()
+            input('Please login before proceed! Press <enter> to continue!')
+        if self.waitLogin():
+            self.logger.debug("Login successful!")
+            self.saveCookie()
         else:
-            logger.info("Login failed!")
+            self.logger.info("Login failed!")
             return False
         return True
 
@@ -317,30 +367,30 @@ class ShopeeWeb(Crawler):
             self.loggedin = self.login()
             yield self
         except Exception as e:
-            logger.error(repr(e))
+            self.logger.error(repr(e))
         finally:
             self.driver.close()
 
     def claimCoin(self):
-        coin_button = self.wait_until("css", "COIN_BUTTON")
+        coin_button = self.waitForClass("COIN_BUTTON")
         if coin_button:
             if has_substr(coin_button.text, '簽到'):
-                logger.info( "Claiming coin for today, " + coin_button.text )
+                self.logger.info( "Claiming coin for today, " + coin_button.text )
                 coin_button.click()
-                coin_button = self.wait_until("css", "COIN_BUTTON")
+                coin_button = self.waitForClass("COIN_BUTTON")
         else:
-            logger.info("Coin button not found!")
+            self.logger.info("Coin button not found!")
             return False
-        coin_value = self.find("css", "COIN_VALUE")
+        coin_value = self.getByClass("COIN_VALUE")
         if coin_value:
             last_value = ''
             # wait while the coin value counting
             while coin_value.text != last_value:
                 last_value = coin_value.text
                 sleep(1)
-            logger.info("目前有 " + coin_value.text + " 蝦幣, " + coin_button.text)
+            self.logger.info("目前有 " + coin_value.text + " 蝦幣, " + coin_button.text)
         else:
-            logger.info("Coin value not found!")
+            self.logger.info("Coin value not found!")
             return False
         return True
 
